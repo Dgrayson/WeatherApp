0d4011e4b74c9e415ce9cbd0f4d23b99d26445b1
Darayavaus Grayson <dgrayson100@gmail.com>
2018-10-23 23:00:53 -0400

    Finished function to pull City name, temp, current condition and icon from JSON.

--- app/src/main/java/com/weather/dgray/weatherapp/CurrentWeather.java
+++ app/src/main/java/com/weather/dgray/weatherapp/CurrentWeather.java
@@ -9,12 +9,15 @@ import android.text.Html;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.squareup.picasso.Picasso;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Iterator;
 
 public class CurrentWeather extends AppCompatActivity {
 
@@ -49,11 +52,47 @@ public class CurrentWeather extends AppCompatActivity {
         double currTemp = jObj.getAsJsonObject("main").get("temp").getAsDouble();
         currTemp = (currTemp - 273.15) * (9/5) + 32;
 
-        tempTextView.setText(Double.toString(currTemp));
-
-        weatherIcon.setText(Html.fromHtml("&#xf01e;"));
+        tempTextView.setText(Double.toString(Math.round(currTemp)));
 
 
+
+        JsonArray currConditionArray = jObj.getAsJsonArray("weather");
+        JsonElement currCondition = currConditionArray.get(0);
+
+
+        currentConditionsTextView.setText(currCondition.getAsJsonObject().get("main").getAsString());
+        weatherIcon.setText(Html.fromHtml(GetWeatherIcon(currCondition.getAsJsonObject().get("icon").getAsString())));
+    }
+
+
+    public String GetWeatherIcon(String icon){
+        String weatherIcon = "";
+
+        switch(icon){
+            case "01d":
+                weatherIcon = "&#xf00d;";
+                break;
+            case "01n":
+                weatherIcon = "&#xf02e;";
+                break;
+            case "03d":
+                weatherIcon = "&#xf002;";
+                break;
+            case "09d":
+                weatherIcon = "&#xf009;";
+                break;
+            case "10d":
+                weatherIcon = "&#xf008;";
+                break;
+            case "10n":
+                weatherIcon = "&#xf036;";
+                break;
+                default:
+                    weatherIcon = "&#xf0dd;";
+                    break;
+        }
+
+        return weatherIcon;
 
     }
 }
